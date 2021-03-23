c2f6df52c8e2348b3ea46268ea77596ac4f653ec
swetshaw <swt.shw94@gmail.com>
2021-03-23 20:40:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const Button = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={props.handleClick}>{props.text}</button>
     </div>
   );
-}
+};
+
+const Display = (props) => {
+  return (
+    <div>
+      <tr>
+        <td>{props.stats}</td> <td>{props.count}</td>
+      </tr>
+    </div>
+  );
+};
+
+const Statistics = (props) => {
+  if (!props.good & !props.bad & !props.neutral) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <table style={{ backgroundColor: "yellow" }}>
+          <Display stats="good" count={props.good}></Display>
+          <Display stats="neutral" count={props.neutral}></Display>
+          <Display stats="bad" count={props.bad}></Display>
+          <Display
+            stats="all"
+            count={props.good + props.neutral + props.bad}
+          ></Display>
+          <Display stats="average" count={props.average()}></Display>
+          <Display
+            stats="positve percentage"
+            count={props.posPercent()}
+          ></Display>
+        </table>
+      </div>
+    );
+  }
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodBtnFn = () => {
+    setGood(good + 1);
+  };
+
+  const neutralBtnFn = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const badBtnFn = () => {
+    setBad(bad + 1);
+  };
+
+  const average = () => {
+    return (good + bad + neutral) / 3;
+  };
+
+  const posPercent = () => {
+    return (good * 100) / (good + bad + neutral);
+  };
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={goodBtnFn} text="Good"></Button>
+      <Button handleClick={neutralBtnFn} text="Neutral"></Button>
+      <Button handleClick={badBtnFn} text="Bad"></Button>
+      <h2>Statistics</h2>
+      <Statistics
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        average={average}
+        posPercent={posPercent}
+      ></Statistics>
+    </div>
+  );
+};
 
 export default App;
